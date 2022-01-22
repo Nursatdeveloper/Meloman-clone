7f09ac7a03b5151d46412f5fde3ca982e6f6e36f
Nursat <47531425+Nursatdeveloper@users.noreply.github.com>
2022-01-22 19:13:38 +0600

    Adding "Age" field to User.cs

--- Meloman-clone/Models/User.cs
+++ Meloman-clone/Models/User.cs
@@ -13,5 +13,6 @@ namespace Meloman_clone.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public int Age {get; set;}
     }
 }
